13cd0d45583394fd93a98d4c950eb3cb18bc2e5b
fmarcosjesus-cpu <f.marcos.jesus@gmail.com>
2026-04-05 12:38:22 -0300

    Add files via upload

--- netlify/functions/get-lessons.js
+++ netlify/functions/get-lessons.js
@@ -51,25 +51,25 @@ exports.handler = async (event) => {
         title: "Aula 1 – Fundamentos da Mira e Introdução ao Curso",
         description: "Base inicial do curso, com os fundamentos da mira e a introdução ao método.",
         embed: "https://iframe.mediadelivery.net/embed/625484/5fd1f8c8-d240-4e44-a7b1-1e667f9fbd12",
-        thumb: "https://vz-da02adc7-ceb.b-cdn.net/5fd1f8c8-d240-4e44-a7b1-1e667f9fbd12/thumbnail_653b3e67.jpg"
+        thumb: "https://vz-da02adc7-ceb.b-cdn.net/5fd1f8c8-d240-4e44-a7b1-1e667f9fbd12/thumbnail_5e215b90.jpg"
       },
       {
         title: "Aula 2 – Ajuste de Mira: A Técnica para Encaçapar com Precisão",
         description: "Aprenda a ajustar a mira com mais precisão e consistência nas jogadas.",
         embed: "https://iframe.mediadelivery.net/embed/625484/144e3546-9a1a-4096-a7c4-fdfeffbecda0",
-        thumb: "https://vz-da02adc7-ceb.b-cdn.net/144e3546-9a1a-4096-a7c4-fdfeffbecda0/thumbnail_688265d6.jpg"
+        thumb: "https://vz-da02adc7-ceb.b-cdn.net/144e3546-9a1a-4096-a7c4-fdfeffbecda0/thumbnail_711bb149.jpg"
       },
       {
         title: "Aula 3 – Efeitos: Domínio Total da Bola Branca",
         description: "Entenda os efeitos e domine o controle da bola branca com mais segurança.",
         embed: "https://iframe.mediadelivery.net/embed/625484/6633a79e-587e-4c0b-9309-5030630f560f",
-        thumb: "https://vz-da02adc7-ceb.b-cdn.net/6633a79e-587e-4c0b-9309-5030630f560f/thumbnail_f07f7dff.jpg"
+        thumb: "https://vz-da02adc7-ceb.b-cdn.net/6633a79e-587e-4c0b-9309-5030630f560f/thumbnail_28ac7846.jpg"
       },
       {
         title: "Aula 4 – Planejamento e Estratégia: A Arte de Não Errar",
         description: "Desenvolva visão de jogo, planejamento e estratégia para reduzir erros.",
         embed: "https://iframe.mediadelivery.net/embed/625484/2d80449b-836a-4a12-990e-0e2fb2d7f5f8",
-        thumb: "https://vz-da02adc7-ceb.b-cdn.net/2d80449b-836a-4a12-990e-0e2fb2d7f5f8/thumbnail_39e378c5.jpg"
+        thumb: "https://vz-da02adc7-ceb.b-cdn.net/2d80449b-836a-4a12-990e-0e2fb2d7f5f8/thumbnail_a2b4b6da.jpg"
       },
       {
         title: "Aula 5 – Técnica Avançada para Memorizar e Aplicar os Efeitos",
